1e3a3f2c3a27e603b1e4b95c80fefbda3a95db02
Nishat Taaha <73392394+NishatTaaha@users.noreply.github.com>
2022-12-24 19:48:15 +0600

    Update queue.cpp

--- Stack & Queue/queue.cpp
+++ Stack & Queue/queue.cpp
@@ -1,64 +1,74 @@
 #include<iostream>
 using namespace std;
 #define n 20
-
 class queue{
     int *arr;
     int front;
-    int back;
+    int rear;
     public:
     queue(){
         arr=new int[n];
         front=-1;
-        back=-1;
+        rear=-1;
     }
-
     void push(int x){
-        if(back==n-1){
+        if(rear==n-1){
             cout<<"Queue Overflow"<<endl;
             return;
         }
-        back++;
-        arr[back]=x;
+        rear++;
+        arr[rear]=x;
         if(front==-1){
             front++;
         }
     }
-
     void pop(){
-        if(front==-1 || front>back){
+        if(front==-1 || front>rear){
             cout<<"No Element in queue!";
             return;
         }
         front++;
     }
-
     int peek(){
-        if(front==-1 || front>back){
+        if(front==-1 || front>rear){
             cout<<"No Element In Queue!"<<endl;
         }
         return arr[front];
     }
-    
     bool empty(){
-        if(front==-1 || front>back){
+        if(front==-1 || front>rear){
             return true;
         }
         return false;
     }
+    void display(){
+        int i;
+        if(empty()){
+            cout<<endl<<"Empty queue"<<endl;
+        }
+        else{
+            cout<<endl;
+            for(i=front;i<=rear;i++){
+                cout<<arr[i]<<" ";
+            }
+        }
+    }
+    
 };
 int main(){
     queue q;
     q.push(1);
     q.push(2);
     q.push(3);
-    cout<<q.peek()<<endl;
-    q.pop();
-    cout<<q.peek()<<endl;
-    q.pop();
-    cout<<q.peek()<<endl;
-    q.pop();
-    cout<<q.empty()<<endl;
+    // q.pop();
+    q.display();
+    // cout<<q.peek()<<endl;
+    // q.pop();
+    // cout<<q.peek()<<endl;
+    // q.pop();
+    // cout<<q.peek()<<endl;
+    // q.pop();
+    // cout<<q.empty()<<endl;
     return 0;
 }
 
